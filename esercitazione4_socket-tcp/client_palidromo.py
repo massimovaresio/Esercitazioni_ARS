596a02244980d36771d7f261d54f86d493c91190
massimovaresio <varesio.massimo@gmail.com>
2025-04-01 13:06:59 +0200

    Add file palindrome

--- esercitazione4_socket-tcp/client_palidromo.py
+++ esercitazione4_socket-tcp/client_palidromo.py
@@ -1,10 +1,22 @@
 from socket import *
-serverName = 'localhost'
+
+serverName = 'localhost'  # Cambia con l'IP del server se in rete
 serverPort = 12000
 clientSocket = socket(AF_INET, SOCK_STREAM)
-clientSocket.connect((serverName,serverPort))
-sentence = input('Scrivi una frase in minuscolo:')
+clientSocket.connect((serverName, serverPort))
+
+# Inserimento della stringa da verificare
+sentence = input('Inserisci una stringa per verificare se è un palindromo: ')
 clientSocket.send(sentence.encode())
-modifiedSentence = clientSocket.recv(1024)
-print ('Dal Server:', modifiedSentence.decode())
+
+# Ricezione del risultato dal server e conversione in booleano
+response = clientSocket.recv(1024).decode()
+# Convertire la stringa ricevuta in booleano
+if response == "True":
+    is_palindrome = True
+else:
+    is_palindrome = False
+
+print(f"Il server dice: {is_palindrome}")
+
 clientSocket.close()
--- esercitazione4_socket-tcp/server_palindromo.py
+++ esercitazione4_socket-tcp/server_palindromo.py
@@ -1,12 +1,25 @@
 from socket import *
+
 serverPort = 12000
-serverSocket = socket(AF_INET,SOCK_STREAM)
-serverSocket.bind(('localhost',serverPort))
+serverSocket = socket(AF_INET, SOCK_STREAM)
+serverSocket.bind(('localhost', serverPort))
 serverSocket.listen(1)
 print('The server is ready to receive')
+
 while True:
     connectionSocket, addr = serverSocket.accept()
+    print(f"Connessione da {addr}")
+
+    # Riceve la stringa dal client
     sentence = connectionSocket.recv(1024).decode()
-    capitalizedSentence = sentence.upper()
-    connectionSocket.send(capitalizedSentence.encode())
+    
+    # Controlla se è un palindromo con if e restituisce un valore booleano
+    if sentence == sentence[::-1]:
+        is_palindrome = True
+    else:
+        is_palindrome = False
+
+    # Invia il valore booleano al client
+    connectionSocket.send(str(is_palindrome).encode())  # Convertito in stringa per l'invio perché con il socket si possono inviare solo interi o stringhe
+
     connectionSocket.close()
